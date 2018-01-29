17ef2466f30be3ede40db85cebe3f24d15bbcf0a
dkznikolaj <nikolajgiliamsen@hotmail.dk>
2018-01-29 11:03:20 +0100

    Made a thingy

---
+++ Mainthingy/Mainthingy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mainthingy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
